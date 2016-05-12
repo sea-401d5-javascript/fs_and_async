1a8ba7c591d62568ac2b3bae2a66bd1827e7db95
Jeff Eng <jeff@jeffeng.me>
2016-05-12 12:56:00 -0700

    added event emitters and listeners however getting syntax error. will debug later

--- jeff_eng/file_read.js
+++ jeff_eng/file_read.js
@@ -1,4 +1,6 @@
+const EventEmitter = require('events').EventEmitter;
 const fs = require('fs');
+const ee = new EventEmitter();
 
 var fileText;
 
@@ -6,19 +8,24 @@ fs.readFile('./one.txt', (err, data) => {
   if (err) return console.log(err);
   fileText = data.toString('hex', 0, 16);
   console.log(fileText, 'first text file');
+  ee.emit('test-event', (data) => {
+    console.log('EVENT EMITTED ' + data);
+  });
+});
 
-  //emit an event that says first file is done
-})
+ee.on('test-event', (data) => {
+  fs.readFile('./two.txt', (err, data) => {
+    if (err) return console.log(err);
+    fileText = data.toString('hex', 0, 16);
+    console.log(fileText, 'second text file');
+    ee.emit('second-test-event', (data) => {
+      console.log('SECOND EVENT EMITTED ' + data);
+  });
+});
 
-//here we need to listen for the first file is done reading, then say second file is done
-fs.readFile('./two.txt', (err, data) => {
-  if (err) return console.log(err);
-  fileText = data.toString('hex', 0, 16);
-  console.log(fileText, 'second text file');
-})
-
-fs.readFile('./three.txt', (err, data) => {
-  if (err) return console.log(err);
-  fileText = data.toString('hex', 0, 16);
-  console.log(fileText, 'third text file');
-})
+ee.on('second-test-event', (data) => {
+  fs.readFile('./three.txt', (err, data) => {
+    if (err) return console.log(err);
+    fileText = data.toString('hex', 0, 16);
+    console.log(fileText, 'third text file');
+  });
