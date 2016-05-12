f3c429df3349177af2eb87b2b7f42cf2a0c68d42
Stephen Schroeder <schrode50@gmail.com>
2016-05-12 12:57:59 -0700

    i got something but not much

---
+++ Stephen_Schroeder/test/test.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const main = require('../main');
+
+describe('readFile processes', () => {
+  it('should have an 8 byte value of hex', () => {
+    fs.readFile(__dirname + '/../one.txt', () => {
+      expect()
+    })
+  })
+})
