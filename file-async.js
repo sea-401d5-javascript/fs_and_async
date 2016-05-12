ccd9c5ede8c38f92d58f54e42d64e759d3fdfffd
Dan Stineback <dstineback@gmail.com>
2016-05-12 11:58:20 -0700

    added all the files and remembered to do a commit

---
+++ file-async.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const EventEmitter = require('events').EventEmitter;
+const ee = new EventEmitter();
+
+
+fs.readFile('./one.txt', (err, data) => {
+  console.log(data.toString('hex',0,8));
+  ee.emit('file-two', data);
+})
+
+ee.on('file-two', () => {
+  fs.readFile('./two.txt', (err, data) => {
+    console.log(data.toString('hex',0,8));
+    ee.emit('file-three', data);
+  })
+})
+
+
+ee.on('file-three', () => {
+  fs.readFile('./three.txt', (err, data) => {
+
+    console.log(data.toString('hex',0,8));
+  })
+
+})
