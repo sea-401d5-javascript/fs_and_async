6934e997c27d4cc4397c3f998f8df189a82cfc3c
Stephen Schroeder <schrode50@gmail.com>
2016-05-13 09:44:14 -0700

    some tweeks

--- Stephen_Schroeder/main.js
+++ Stephen_Schroeder/main.js
@@ -1,35 +1,36 @@
-const main = exports = module.exports;
+'use strict';
 const fs = require('fs');
-const EventEmitter = require('events').EventEmitter;
-const ee = new EventEmitter();
+const ee = new(require('events').EventEmitter);
 
 var fileList = ['text_files/one.txt', 'text_files/two.txt', 'text_files/three.txt'];
 
-var finalArray = [];
-
-fs.readFile('text_files/one.txt', (err, data) => {
-  console.log(data.toString('hex', 0, 8));
-  finalArray.push(0);
-  ee.emit('one');
-})
-
-ee.on('one', () => {
-  fs.readFile('text_files/two.txt', (err, data) => {
+var mainReader = module.exports = function(cb) {
+  let finalArray = [];
+  fs.readFile('text_files/one.txt', (err, data) => {
     console.log(data.toString('hex', 0, 8));
-    finalArray.push(1);
-    ee.emit('two');
-  })
-})
+    finalArray.push(0);
+    ee.emit('one');
+  });
 
-ee.on('two', () => {
-  fs.readFile('text_files/three.txt', (err, data) => {
-    console.log(data.toString('hes', 0, 8));
-    finalArray.push(2);
-  })
-})
+  ee.on('one', () => {
+    fs.readFile('text_files/two.txt', (err, data) => {
+      console.log(data.toString('hex', 0, 8));
+      finalArray.push(1);
+      ee.emit('two');
+    });
+  });
 
-main.runIt = () => {
-  return finalArray;
+  ee.on('two', () => {
+    fs.readFile('text_files/three.txt', (err, data) => {
+      console.log(data.toString('hex', 0, 8));
+      finalArray.push(2);
+      cb.files
+    })
+  })
 }
+
+mainReader((finalArray) => {
+  return finalArray;
+})
 
 debugger;
--- Stephen_Schroeder/test/test.js
+++ Stephen_Schroeder/test/test.js
@@ -1,10 +1,38 @@
-const chai = require('chai');
-const main = require('../main');
+'use strict';
+
+const expect = require('chai').expect;
+const mainReader = require('../main.js');
+const fs = require('fs');
 
 describe('readFile processes', () => {
-  it('should have an 8 byte value of hex', () => {
-    fs.readFile(__dirname + '/../one.txt', () => {
-      expect()
-    })
-  })
-})
+  let files;
+  before((done) => {
+    files = [];
+    fs.readFile(__dirname + '/../text_files/one.txt', (err, data) => {
+      console.log('one');
+      files.push(data.slice(0, 8));
+      fs.readFile(__dirname + '/../text_files/two.txt', (err, data) => {
+        console.log('two');
+        files.push(data.slice(0, 8));
+        fs.readFile(__dirname + '/../text_files/three.txt', (err, data) => {
+          console.log('three');
+          files.push(data.slice(0, 8));
+          done();
+        });
+      });
+    });
+  });
+
+  it('should read files correctly', () => {
+    mainReader((files) => {
+      expect(Buffer.isBuffer(files[0])).to.equal(true);
+      done();
+    });
+  });
+
+  it('should keep files in correct order', (done) => {
+    mainReader((data) => {
+      expect(data[0].toString()).to.eql(files[0].toString());
+    });
+  });
+});
