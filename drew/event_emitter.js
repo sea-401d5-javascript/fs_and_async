00c58e284d179023104110d1f46d2c7e245b1d86
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-05-19 16:49:10 -0700

    submitting with changes for jeff

--- drew/event_emitter.js
+++ drew/event_emitter.js
@@ -5,32 +5,31 @@ const ee = new (require('events').EventEmitter)
 var reader = module.exports = function(cb) {
   let files = [];
   ee.on('file-one-read', (data) => {
-    fs.readFile('./two.txt', (err,data) => {
-      ee.emit('file-two-read', data)
-    })
     var oneData = data.toString();
     var bufferOneData = new Buffer(oneData);
     var bufferOneDataStringed = bufferOneData.toString('hex');
     var bufferOneDataStringedSliced = bufferOneDataStringed.slice(0,8);
     console.log('the first 8 bytes of file one read: ', bufferOneDataStringedSliced);
     files.push(bufferOneDataStringedSliced)
+    fs.readFile('./two.txt', (err,data) => {
+      ee.emit('file-two-read', data)
+    })
   })
 
   fs.readFile('./one.txt', (err,data) => {
-    console.log(data);
     ee.emit('file-one-read', data)
   })
 
   ee.on('file-two-read', (data) => {
-    fs.readFile('./three.txt', (err,data) => {
-      ee.emit('file-three-read', data)
-    })
     var twoData = data.toString();
     var bufferTwoData = new Buffer(twoData);
     var bufferTwoDataStringed = bufferTwoData.toString('hex');
     var bufferTwoDataStringedSliced = bufferTwoDataStringed.slice(0,8);
     console.log('the first 8 bytes of file two read: ', bufferTwoDataStringedSliced);
     files.push(bufferTwoDataStringedSliced)
+    fs.readFile('./three.txt', (err,data) => {
+      ee.emit('file-three-read', data)
+    })
   })
 
   ee.on('file-three-read', (data) => {
@@ -44,8 +43,3 @@ var reader = module.exports = function(cb) {
   })
 
 }
-
-// fs.readFile('./one.txt', (err,data) => {
-//   console.log(data);
-//   ee.emit('file-one-read', data)
-// })
--- drew/test/read_test.js
+++ drew/test/read_test.js
@@ -13,31 +13,25 @@ describe('file reading tests', function() {
       var bufferOneData = new Buffer(oneData);
       var bufferOneDataStringed = bufferOneData.toString('hex');
       var bufferOneDataStringedSliced = bufferOneDataStringed.slice(0,8);
-      console.log('the first 8 bytes of file one read: ', bufferOneDataStringedSliced);
+
       files.push(bufferOneDataStringedSliced)
       fs.readFile(__dirname + '/../two.txt', (err, data) => {
         var twoData = data.toString();
         var bufferTwoData = new Buffer(twoData);
         var bufferTwoDataStringed = bufferTwoData.toString('hex');
         var bufferTwoDataStringedSliced = bufferTwoDataStringed.slice(0,8);
-        console.log('the first 8 bytes of file two read: ', bufferTwoDataStringedSliced);
+
         files.push(bufferTwoDataStringedSliced)
         fs.readFile(__dirname + '/../three.txt', (err, data) => {
           var threeData = data.toString();
           var bufferThreeData = new Buffer(threeData);
           var bufferThreeDataStringed = bufferThreeData.toString('hex');
           var bufferThreeDataStringedSliced = bufferThreeDataStringed.slice(0,8);
-          console.log('the first 8 bytes of file three read: ', bufferThreeDataStringedSliced);
+
           files.push(bufferThreeDataStringedSliced)
           done();
         });
       });
-    });
-  });
-
-  it('should test async', (done) => {
-    process.nextTick(() => {
-      done();
     });
   });
 
